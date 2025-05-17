e7d0a3f4c0f144b7a3ea5e69839270bc208b2b33
Mercy <maritimmercy8@gmail.com>
2025-05-17 15:05:43 +0300

    added init

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,22 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        self.name = name
+        Author.all.append(self)
+        
 
 
 class Book:
-    pass
-
-
+    all = []
+    
+def __init__(self, title):
+    if not isinstance(title):
+        raise Exception("Title must be a string")
+        self.title = title
+        Book.all.append(self)
+         
 class Contract:
-    pass
+   
+        
